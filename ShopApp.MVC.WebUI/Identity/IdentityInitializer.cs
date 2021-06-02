3c60f6e819d2993acfce070068757d2f1aeb4001
Erdinç <erdinckiyik@gmail.com>
2021-06-02 18:56:30 +0300

    secondcommit

---
+++ ShopApp.MVC.WebUI/Identity/IdentityInitializer.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using ShopApp.MVC.WebUI.Entity;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ShopApp.MVC.WebUI.Identity
+{
+    public class IdentityInitializer:CreateDatabaseIfNotExists<IdentityDataContext>
+    {
+        protected override void Seed(IdentityDataContext context)
+        {
+
+            // Rolleri
+            if (!context.Roles.Any(i => i.Name == "admin"))
+            {
+                var store = new RoleStore<ApplicationRole>(context);
+                var manager = new RoleManager<ApplicationRole>(store);
+                var role = new ApplicationRole() { Name = "admin", Description = "admin rolü" };
+                manager.Create(role);
+            }
+            if (!context.Roles.Any(i => i.Name == "user"))
+            {
+                var store = new RoleStore<ApplicationRole>(context);
+                var manager = new RoleManager<ApplicationRole>(store);
+                var role = new ApplicationRole() { Name = "user", Description = "user rolü" }; ;
+                manager.Create(role);
+            }
+
+            if (!context.Users.Any(i => i.Name == "erdinckiyik"))
+            {
+                var store = new UserStore<ApplicationUser>(context);
+                var manager = new UserManager<ApplicationUser>(store);
+                var user = new ApplicationUser() { Name = "erdinç", Surname = "kiyik", UserName = "erdinck", Email = "erdinckiyik@gmail.com" };
+
+                manager.Create(user, "1234567");
+                manager.AddToRole(user.Id, "admin");
+                manager.AddToRole(user.Id, "user");
+            }
+
+            if (!context.Users.Any(i => i.Name == "grup10"))
+            {
+                var store = new UserStore<ApplicationUser>(context);
+                var manager = new UserManager<ApplicationUser>(store);
+                var user = new ApplicationUser() { Name = "grup", Surname = "10", UserName = "grup10", Email = "grup10@gmail.com" };
+
+                manager.Create(user, "1234567");
+                manager.AddToRole(user.Id, "user");
+            }
+            base.Seed(context);
+        }
+    }
+}
